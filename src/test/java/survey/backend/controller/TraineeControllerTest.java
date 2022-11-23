197d59facb3cffb36bc2b275657ac7efb4f49d0c
loicgardes <loic.gardes@gmail.com>
2022-11-23 10:42:21 +0100

    Start test Trainee Controller

---
+++ src/test/java/survey/backend/controller/TraineeControllerTest.java
@@ -0,0 +1,14 @@
+package survey.backend.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TraineeControllerTest {
+    @Test
+    void TestDefault(){
+
+    }
+
+
+}
